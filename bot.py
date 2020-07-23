8223bbc90d7ffa8bb7caa0bd377eea38948c745e
brynphillips <17993329+brynphillips@users.noreply.github.com>
2020-07-23 11:16:53 -0700

    refactor and more commands

--- bot.py
+++ bot.py
@@ -1,15 +1,11 @@
-#this is a bot
+# this is a bot
+# import os  # for importing env vars for the bot to use
+import os
+from datetime import datetime
 
-import os # for importing env vars for the bot to use
-import requests
-import json
-import re
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from datetime import date
-from datetime import datetime
 from twitchio.ext import commands
-
 
 sp = None
 
@@ -20,7 +16,7 @@ bot = commands.Bot(
     client_id=os.environ['CLIENT_ID'],
     nick=os.environ['BOT_NICK'],
     prefix=os.environ['BOT_PREFIX'],
-    initial_channels=[os.environ['CHANNEL']]
+    initial_channels=[os.environ['CHANNEL']],
 )
 
 
@@ -29,8 +25,9 @@ bot = commands.Bot(
 async def event_ready():
     'Called once when the bot goes online.'
     print(f'{os.environ["BOT_NICK"]} is online!')
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    #await ws.send_privmsg(os.environ['CHANNEL'], f'/me has landed!')
+    # ws = bot._ws  # this is only needed to send messages within event_ready
+    # await ws.send_privmsg(os.environ['CHANNEL'], f'/me has landed!')
+
 
 @bot.event
 async def event_message(ctx):
@@ -42,24 +39,27 @@ async def event_message(ctx):
 
     await bot.handle_commands(ctx)
 
+
 @bot.command(name='ohai')
 async def greeting(ctx):
     await ctx.send(f'Oh hey there @{ctx.author.name}')
 
+
 @bot.command(name='help')
 async def help(ctx):
-    await ctx.send(f'Commands: !help, !ohai')
+    await ctx.send('Commands: !help, !ohai, !song, !artistslink')
+
 
 @bot.command(name='so')
 async def so(ctx):
-    if ctx.author.is_mod == False:
-        return
-    else:
-        await ctx.send(f'You should go check out @{ctx.content[5:]} at https://www.twitch.tv/{ctx.content[5:]}')
+    if ctx.author.is_mod:
+        await ctx.send(f'You should go check out @{ctx.content[5:]}, \
+        at https://www.twitch.tv/{ctx.content[5:]}')
+
 
 @bot.command(name='followage')
 async def followage(ctx):
-    otherusers = await bot.get_users(ctx.content.split(" ")[1])
+    otherusers = await bot.get_users(ctx.content.split(' ')[1])
     followage = await bot.get_follow(otherusers[0].id, '29998625')
     converted = datetime.fromisoformat(followage['followed_at'][:-1])
     currtime = datetime.utcnow()
@@ -67,75 +67,84 @@ async def followage(ctx):
     print(result)
     numyears = result.days//365
     numdays = result.days % 365
-   # print(dir(result))
-   # print(result.total_seconds())
 
     if numyears < 1 and numdays < 1 and result.total_seconds()//60//60 < 1:
-        await ctx.send(f'{otherusers[0].display_name} has been following for {int(result.total_seconds()//60)} minutes.')
+        await ctx.send(f'{otherusers[0].display_name} has, \
+        been following for {int(result.total_seconds()//60)} minutes.')
     elif numyears < 1 and numdays < 1:
-        await ctx.send(f'{otherusers[0].display_name} has been following for {int(result.total_seconds()//60//60)} hours.')
+        await ctx.send(f'{otherusers[0].display_name} has, \
+        been following for {int(result.total_seconds()//60//60)} hours.')
     elif numyears < 1:
-        await ctx.send(f'{otherusers[0].display_name} has been following for {numdays} days.')
+        await ctx.send(f'{otherusers[0].display_name} has, \
+        been following for {numdays} days.')
     elif numyears == 1:
-        await ctx.send(f'{otherusers[0].display_name} has been following for a year and {numdays} days.')
+        await ctx.send(f'{otherusers[0].display_name} has, \
+        been following for a year and {numdays} days.')
     elif numyears > 1:
-        await ctx.send(f'{otherusers[0].display_name} has been following for {numyears} years and {numdays} days.')
+        await ctx.send(f'{otherusers[0].display_name} has, \
+        been following for {numyears} years and {numdays} days.')
+
 
 @bot.command(name='slow')
 async def slow(ctx):
     print(ctx.author.is_mod)
-    if ctx.author.is_mod == False:
-        return
-    else:
+    if ctx.author.is_mod:
         await ctx.slow()
+
 
 @bot.command(name='unslow')
 async def unslow(ctx):
     print(ctx.author.is_mod)
-    if ctx.author.is_mod == False:
-        return
-    else:
+    if ctx.author.is_mod:
         await ctx.unslow()
+
 
 @bot.command(name='song')
 async def song(ctx):
 
-   # headers = {
-   # 'Accept': 'application/json',
-   # 'Content-Type': 'application/json',
-   # 'Authorization': 'Bearer ' + str(os.environ['SPOTIFY_TOKEN']),
-   # }
+    result = sp.currently_playing()
+    artists = ', '.join([x['name'] for x in result['item']['artists']])
+    song = result['item']['name']
 
-    #response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
-    #print(response)
-    #song = response.json()['item']['name']
-    #artists = ", ".join([x['name'] for x in response.json()['item']['artists']])
+    await ctx.send(f'Artist: {artists} | Title: {song}')
+
+
+@bot.command(name='artistslink')
+async def artistslink(ctx):
 
     result = sp.currently_playing()
-    for items in result['items']:
+    links = ', '.join([
+        x['external_urls']['spotify']
+        for x in result['item']['artists']
+    ])
 
-    #await ctx.send(f'Artist: {artists} | Title: {song}')
-    #The way I got this to work before was to grab client secret
-    #and add to env seperatly. Expires in 1 hour though
+    await ctx.send(f'Here\'s a link for the arsits[s]: {links}')
+
 
 def main():
-
     global sp
     sp = spotipyinit()
     bot.run()
 
+
 def spotipyinit():
 
-    CLIENT=os.environ['SPOTIPY_CLIENT_ID']
-    SECRET=os.environ['SPOTIPY_CLIENT_SECRET']
-    REDIRECT=os.environ['SPOTIPY_REDIRECT_URI']
-    USERNAME=os.environ['SPOTIPY_USERNAME']
+    CLIENT = os.environ['SPOTIPY_CLIENT_ID']
+    SECRET = os.environ['SPOTIPY_CLIENT_SECRET']
+    REDIRECT = os.environ['SPOTIPY_REDIRECT_URI']
+    USERNAME = os.environ['SPOTIPY_USERNAME']
 
     scope = 'user-read-currently-playing'
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username=USERNAME, client_id=CLIENT, client_secret=SECRET, redirect_uri=REDIRECT, scope=scope))
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            username=USERNAME, client_id=CLIENT, client_secret=SECRET,
+            redirect_uri=REDIRECT, scope=scope,
+        ),
+    )
 
     return sp
+
 
 if __name__ == '__main__':
     exit(main())
