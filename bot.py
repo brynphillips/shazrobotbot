ddc0379e3e132d4be8efcbbeaa14cb84e6bd2348
brynphillips <17993329+brynphillips@users.noreply.github.com>
2020-07-30 13:17:34 -0700

    Changed a bunch of stuff and added logging

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 # this is a bot
 # import os  # for importing env vars for the bot to use
 import os
+import inspect
+import json
+import logging
+import logging.config
+from logging.handlers import RotatingFileHandler
 from datetime import datetime
 from time import perf_counter
 
@@ -12,7 +17,7 @@ from twitchio.ext import commands
 
 class Bot(commands.Bot):
 
-    def __init__(self):
+    def __init__(self, logger):
         super().__init__(
             # set up the bot
             irc_token=os.environ['TMI_TOKEN'],
@@ -25,7 +30,10 @@ class Bot(commands.Bot):
         self.sp = spotipyinit()
         self.counter = 0
         self.motd = ''
-
+        self.uptime = 0
+        self.id = 29998625
+        self.name = 'Shazrobot'
+        self.logger = logger
     # bot.py, below bot objects
 
     async def event_ready(self):
@@ -45,21 +53,28 @@ class Bot(commands.Bot):
         await self.handle_commands(ctx)
 
     @commands.command(name='ohai')
-    async def greeting(self, ctx):
+    async def ohai(self, ctx):
         await ctx.send(f'Oh hey there @{ctx.author.name}')
 
     @commands.command(name='help')
     async def help(self, ctx):
-        await ctx.send('Commands: !help, !ohai, !song, !artistslink')
+        commands_list = list(inspect.getmembers(self)[2][1]['commands'].keys())
+        commands_mod = ['so', 'slow', 'unslow']
+        commands_semifinal = list(set(commands_list) - set(commands_mod))
+        commands_final = ", ".join(commands_semifinal)
+        self.logger.info(f'{ctx.author.display_name} used the command.')
+        await ctx.send(f'Commands: {commands_final}')
 
     @commands.command(name='so')
     async def so(self, ctx):
         if ctx.author.is_mod:
+            self.logger.info(f'{ctx.author.display_name} used the command.')
             await ctx.send(f'You should go check out @{ctx.content[5:]}, \
             at https://www.twitch.tv/{ctx.content[5:]}')
 
     @commands.command(name='followage')
     async def followage(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
         part = ctx.content.partition(' ')[2].replace('@', '')
         if part == '':
             followage = await self.get_follow(ctx.author.id, '29998625')
@@ -108,42 +123,26 @@ class Bot(commands.Bot):
 
     @commands.command(name='slow')
     async def slow(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
         print(ctx.author.is_mod)
         if ctx.author.is_mod:
             await ctx.slow()
 
     @commands.command(name='unslow')
     async def unslow(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
         print(ctx.author.is_mod)
         if ctx.author.is_mod:
             await ctx.unslow()
 
-    @commands.command(name='song')
-    async def song(self, ctx):
-
-        result = self.sp.currently_playing()
-        artists = ', '.join([x['name'] for x in result['item']['artists']])
-        song = result['item']['name']
-
-        await ctx.send(f'Artist: {artists} | Title: {song}')
-
-    @commands.command(name='artist')
-    async def artist(self, ctx):
-
-        result = self.sp.currently_playing()
-        links = ', '.join([
-            x['external_urls']['spotify']
-            for x in result['item']['artists']
-        ])
-
-        await ctx.send(f'Here\'s a link for the artist(s): {links}')
-
     @commands.command(name='ping')
     async def ping(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
         await ctx.send(f'pong {ctx.content[5:]}')
 
     @commands.command(name='title')
     async def title(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
         if ctx.author.is_mod:
             async with httpx.AsyncClient() as client:
                 headers = {
@@ -158,8 +157,49 @@ class Bot(commands.Bot):
                     data={'title': ctx.content.split(' ', 1)[1]},
                 )
 
+    @commands.command(name='motd')
+    async def motd(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
+        if ctx.content.partition(' ')[2]:
+            self.motd = ctx.content.partition(' ')[2]
+            await ctx.send(
+                f'{ctx.author.name} has set the !motd as '
+                f'"{self.motd}"',
+            )
+        else:
+            await ctx.send(f'{self.motd}')
+
+    @commands.command(name='uptime')
+    async def uptime(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
+        uptime = await self.get_stream(self.id)
+        timedelta = (datetime.utcnow()
+        - datetime.fromisoformat(uptime['started_at'][:-1]))
+        total_seconds = timedelta.total_seconds()
+        hours = total_seconds//60//60
+        if total_seconds//60//60 >= 1:
+            await ctx.send(
+                f'{self.name} has been live '
+                f'for {int(total_seconds//60//60)} hours and '
+                f'{int(total_seconds//60 - (60*hours))} minutes. PogChamp',
+            )
+        elif total_seconds//60//60 <= 1:
+            await ctx.send(
+                f'{self.name} has been live '
+                f'for {total_seconds//60} minutes. PogChamp',
+            )
+
+    @commands.command(name='followers')
+    async def followers(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
+        followers = await self.get_followers(self.id, count = True)
+        await ctx.send(f'{self.name} has {followers} number of follows!')
+
+# Spotify commands
+
     @commands.command(name='nextsong')
     async def nextsong(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
         starttime = perf_counter()
         self.counter += 1
         while self.counter >= 2 and perf_counter()-starttime <= 5:
@@ -168,25 +208,45 @@ class Bot(commands.Bot):
 
     @commands.command(name='playlist')
     async def playlist(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
         currplaylist = self.sp.current_user_playlists(1, 0)
         playlisturl = currplaylist['items'][0]['external_urls']['spotify']
         await ctx.send(f'Playlist: {playlisturl}')
 
-    @commands.command(name='motd')
-    async def motd(self, ctx):
-        if ctx.content.partition(' ')[2]:
-            self.motd = ctx.content.partition(' ')[2]
-            await ctx.send(
-                f'{ctx.author} has set the !motd as '
-                f'{self.motd}',
-            )
-        else:
-            await ctx.send(f'{self.motd}')
+    @commands.command(name='song')
+    async def song(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
+        result = self.sp.currently_playing()
+        artists = ', '.join([x['name'] for x in result['item']['artists']])
+        song = result['item']['name']
+
+        await ctx.send(f'Artist: {artists} | Title: {song}')
+
+    @commands.command(name='artist')
+    async def artist(self, ctx):
+        self.logger.info(f'{ctx.author.display_name} used the command.')
+        result = self.sp.currently_playing()
+        links = ', '.join([
+            x['external_urls']['spotify']
+            for x in result['item']['artists']
+        ])
+        await ctx.send(f'Here\'s a link for the artist(s): {links}')
+
+# Main
 
 
 def main():
-    # change this global variable to something else
-    bot = Bot()
+
+    #logging.config.fileConfig('logging.conf')
+    logger = logging.getLogger('shazrobotbot')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s|%(levelname)s|%(name)s|%(funcName)s|%(message)s')
+    file_handler = RotatingFileHandler('logging.log', maxBytes=1000*1000)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    bot = Bot(logger)
     bot.run()
 
 
